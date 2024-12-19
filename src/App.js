23b1322fc099a100bec9aa24a31924afd9691598
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2024-12-19 16:18:05 +0700

    #Import Navbar,Home,Main,Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './App.css';
-
-function App(){
-  const WhatInput = (<input type="text"/>);
-  const Wherebutton = (<button>ปุ่มกดโง่</button>);
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
+const App =() => {
   return(
-    <dir classname = "App">
-      <h1>TEST Working</h1>
-      <p>{WhatInput}</p>
-      <p>{Wherebutton}</p>
-    </dir>
+    <>
+    <Navbar/>
+    <Home/>
+    <Main/>
+    <Footer/>
+    </>
   ); 
 }
 

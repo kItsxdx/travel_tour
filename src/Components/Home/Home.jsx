287f070f1097fe5014e60406e7dd6cb856bc21b2
kItsxdx <155843650+kItsxdx@users.noreply.github.com>
2024-12-20 17:14:20 +0700

    #import video show content

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,26 @@
-import react from 'react'
+import React from 'react'
 import './home.css'
-
+import video from '../../Assets/video2.mp4';
 const Home = () => {
     return (
-        <dir>home</dir>
+        <section className="home">
+            <div className="overlay"></div>
+            <video src={video} muted autoPlay loop type ="video/mp4"></video>
+
+
+            <div className="homeContent container">
+                <div className="textDiv">
+                    <span className="smallText">
+                        ได้เที่ยวกับแมวของคุณ
+                    </span>
+
+                    <h1 className="homeTitle">
+                        เราคือมิจที่จะพานำเที่ยวไม่ลอยแพรแน่นอน 555
+                    </h1>
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 
